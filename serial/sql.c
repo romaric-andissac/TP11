ae9a78103289b0c92fb2871724d30b428f1ac208
Andissac <andissac.romaric@lyceeppr.fr>
2020-03-12 16:25:53 +0100

    TP11

---
+++ serial/sql.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mariadb/mysql.h>
+
+int main(void)
+{
+    MYSQL *conn;
+    
+    if ((conn = mysql_init(NULL)) == NULL)
+    {
+        fprintf(stderr, "Could not init DB\n");
+        return EXIT_FAILURE;
+    }
+    if (mysql_real_connect(conn, "localhost", "tp11" , "snir", "capteurs", 0, NULL, 0) == NULL)
+    
+    {
+        fprintf(stderr, "DB Connection Error\n");
+        return EXIT_FAILURE;
+    }
+    if (mysql_query(conn, "INSERT INTO capteurs (test) VALUES ('Hello World')") != 0)
+    {
+        fprintf(stderr, "Query Failure\n");
+        return EXIT_FAILURE;
+    }
+    mysql_close(conn);
+    return EXIT_SUCCESS;
+}
